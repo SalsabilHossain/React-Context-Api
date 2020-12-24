c5cac15944e5e214929c3c8cbe713d8c3f4e2775
Salsabil <salsabil2306@gmail.com>
2020-12-24 16:39:25 +0600

    First

---
+++ src/components/BookList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class BookList extends Component{
+    render(){
+    return(
+        <div>
+            <ul>
+                <li>The way of kings</li>
+                <li>The name of the wind</li>
+                <li>The final Empire</li>
+
+            </ul>
+        </div>
+       
+    );
+    }
+}
+
+export default BookList;
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../context/Themecontext';
+
+class Navbar extends Component {
+  // static contextType = ThemeContext;
+  render() {
+    return ( 
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        return (
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        )
+      }}</ThemeContext.Consumer>
+    );
+  }
+}
+ 
+export default Navbar;
